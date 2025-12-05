cb8e2f812ecb5ff17471d027a25391fb4f86e36c
naninani2xx1 <nhchuong2001@gmail.com>
2025-12-05 17:34:24 +0700

    [update]: path for randomExtension

--- src/DependencyInjection/RandomExtension.php
+++ src/DependencyInjection/RandomExtension.php
@@ -3,10 +3,10 @@
 namespace NguyenHoaiChuong\RandomBundle\DependencyInjection;
 
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\Config\FileLocator;
 
 class RandomExtension extends Extension
 {
